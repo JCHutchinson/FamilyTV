1d2383d5ae8a7569f12c25a31a70bde00990efd8
oyiza <epraise17@yahoo.com>
2015-11-27 17:47:36 -0600

    logout.php
    changed the redirect location to homepage of the website

--- www/1353/logout.php
+++ www/1353/logout.php
@@ -4,5 +4,4 @@ session_unset();
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-header("Location: ../index.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
-?>
+header("Location: http://www.familytv.bridgeitsolutions.ca");        // you can enter home page here
