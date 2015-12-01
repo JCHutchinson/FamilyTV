12327283247044a4cd2785187f7eabd2947a5bb3
oyiza <epraise17@yahoo.com>
2015-12-01 16:33:52 -0600

    picking up after fail
    edited profile.php

--- www/profile.php
+++ www/profile.php
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
 <?php
 
-include('header.php');
+include('header2.php');
 //include('./1353/fbconfig.php');
 session_start();
 ?>
-
-
-
-<div id="nav2">
-
-	<p id="logo">FamilyTV</p>
-
-	<ul>
-
-		<li><a href="index.php">Home</a></li>
-
-		<li><a href="index.php">About Us</a></li>
-
-		<li><a href="index.php">The Story</a></li>
-
-		<li><a href="index.php">Section 3</a></li>
-
-		<li><a href="./1353/logout.php">Logout</a></li>
-
-	</ul>
-
-</div>
-
 
 
 <div id="profile-container">
@@ -126,42 +102,3 @@ session_start();
 include('footer.php');
 
 ?>
-
-=======
-<?php
-include('header2.php');
-?>
-<div id="profile-container">
-
-<p id="profile-title">Welcome to FamilyTV</p>
-
-<br />
-<br />
-
-<div class="col-md-3">
-	<img src="img/tempprofile.jpg" class="img-circle" style="width: 20vw;"/>
-</div>
-
-<div class="col-md-3 psection" style="background-color: #E2ECF6">
-	<h3>Firstname LastName</h3> 
-	<h3>Location</h3>
-</div>
-<div class="col-md-3 psection" style="background-color: #74959A">
-	<h3> Check/Edit list of devices</h3>
-	<p>You currently have 0 Devices connected</p>
-	 <input type="submit" value="Submit">
-</div>
-<div class="col-md-3 psection" style="background-color: #E2ECF6;">
-	<h3>Upload an image from desktop</h3>
-	<h3>View Images</h3>
-</div>
-
-<div></div>
-<div></div>
-
-</div><!-- END OF PROFILE CONTAINER -->
-
-<?php
-include('footer.php');
-?>
->>>>>>> origin/master
